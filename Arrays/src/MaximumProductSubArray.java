0ab1119def54c39c7226017d36e00df955800cbb
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-07-20 19:32:57 +0530

    pbc

--- Arrays/src/MaximumProductSubArray.java
+++ Arrays/src/MaximumProductSubArray.java
@@ -1,5 +1,25 @@
 
 public class MaximumProductSubArray {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int ans=nums[0];
+        int min=ans;
+        int max=ans;
+
+        for(int i=1;i<n;i++){
+
+            if(nums[i]<0){
+                int temp=max;
+                max=min;
+                min=temp;
+            }
+
+            max=Math.max(nums[i],max*nums[i]);
+            min=Math.min(nums[i],min*nums[i]);
+            ans=Math.max(ans,max);
+        }
+        return ans;
+    }
     public static void main(String[] args) {
 
     }
