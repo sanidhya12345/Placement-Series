1172a04bdcb8a00675151fdcdc94051a0ebc6e76
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-09-03 19:34:12 +0530

    commit

--- Arrays/src/SortColors.java
+++ Arrays/src/SortColors.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class SortColors {
     public static void main(String[] args) {
+
         Scanner sc=new Scanner(System.in);
         int length=sc.nextInt();
         int [] colors=new int[length];
