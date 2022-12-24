5a8c65253630fb1d949445faf7d47ba04924bbec
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-24 16:32:20 +0530

    program

---
+++ PowerOfFour.java
@@ -0,0 +1,9 @@
+public class PowerOfFour {
+    public static void main(String[] args) {
+        System.out.println((Math.log(5)/Math.log(4))%1);
+        System.out.println(Math.log(5));
+        System.out.println(Math.log(4));
+        System.out.println(1.159%1);
+
+    }
+}
