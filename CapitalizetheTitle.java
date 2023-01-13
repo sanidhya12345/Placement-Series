8efa0fb204d7b1db491ad68c5a1c75c9dc5cfea6
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-13 10:43:33 +0530

    .class files

--- CapitalizetheTitle.java
+++ CapitalizetheTitle.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class CapitalizetheTitle {
+/*
+ * Copyright (c) 2023 by Sanidhya Varshney.
+ */
+
+import java.util.Scanner;
+
+public class CapitalizetheTitle {
+    public static String capitalizeTitle(String title) {
+        String [] titleArray=title.split(" ");
+        String ans="";
+        for(String str:titleArray){
+            if(str.length()==1 || str.length()==2){
+                ans+=str.toLowerCase();
+            }
+            else{
+//                if()
+//                String substring=str.substring(1);
+//                String add=firstLetter+substring;
+//                ans+=add;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        String text="capiTalIze tHe titLe";
+        System.out.println(capitalizeTitle(text));
+    }
 }
